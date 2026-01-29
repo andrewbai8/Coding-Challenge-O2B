f8b5d12308fe53678a9f6e0346a39bf026f53dab
andrew <andrewbai@usf.edu>
2026-01-29 17:42:17 -0500

    Finished Part 2 of All the Inventory Metrics - Stock Deficit / reorder / ETC

--- inventory_assistant.js
+++ inventory_assistant.js
@@ -11,4 +11,9 @@ let reorderLevel = "400"
 let targetStock = "100 on Hand"
 let weeklyDemand = "200 Units Sold Per Week"
 let supplierLeadTimeWeeks = "2 Weeks"
-
+// Part 2 // 
+let weeksOfCover = weeklyDemand > 0 ? currentStock / weeklyDemand : Infinity
+let stockDeficit = Math.max(0, targetStock - currentStock)
+let reorderQuantity = (currentStock <= reorderLevel || weeksOfCover < supplierLeadTimeWeeks) ? Math.ceil(stockDeficit) : 0
+let estimatedReorderCost = reorderQuantity * unitCost
+let reorderNow = currentStock <= reorderLevel || weeksOfCover < supplierLeadTimeWeeks
