2a4067a42254fc8f116eaf325ec582c91ff27dcc
andrew <andrewbai@usf.edu>
2026-01-29 17:39:20 -0500

    Finished Part 1 Of Creating Variables - Unit Cost, Stock, ETC

--- inventory_assistant.js
+++ inventory_assistant.js
@@ -1,3 +1,14 @@
 
 // Comment // 
 console.log("Hello this is the AI Inventory assistant")
+
+
+// Part 1 // 
+let itemName = "USB-C Cable"
+let unitCost = "3.25"
+let currentStock = "100"
+let reorderLevel = "400"
+let targetStock = "100 on Hand"
+let weeklyDemand = "200 Units Sold Per Week"
+let supplierLeadTimeWeeks = "2 Weeks"
+
